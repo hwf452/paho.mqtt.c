7aa674ba8cef9d5556e8f43fc10a4ef2dab326ae
hwf452 <hwf452@163.com>
2021-08-21 11:39:00 +0800

    publish.c change

--- src/samples/MQTTClient_publish.c
+++ src/samples/MQTTClient_publish.c
@@ -19,21 +19,30 @@
 #include <string.h>
 #include "MQTTClient.h"
 
-#define ADDRESS     "tcp://mqtt.eclipse.org:1883"
+#define ADDRESS     "tcp://39.101.173.20:1883"
 #define CLIENTID    "ExampleClientPub"
-#define TOPIC       "MQTT Examples"
+#define TOPIC       "push"
 #define PAYLOAD     "Hello World!"
 #define QOS         1
 #define TIMEOUT     10000L
 
 int main(int argc, char* argv[])
 {
+    //声明一个MQTTClient
     MQTTClient client;
+    //添加的用户名
+    char *username= "admin"; 
+    //添加的密码
+    char *password = "public"; 
+    //初始化MQTT Client选项
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    //#define MQTTClient_message_initializer { {'M', 'Q', 'T', 'M'}, 0, 0, NULL, 0, 0, 0, 0 }
     MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    //声明消息token
     MQTTClient_deliveryToken token;
     int rc;
 
+    //使用参数创建一个client，并将其赋值给之前声明的client
     if ((rc = MQTTClient_create(&client, ADDRESS, CLIENTID,
         MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS)
     {
@@ -43,16 +52,25 @@ int main(int argc, char* argv[])
 
     conn_opts.keepAliveInterval = 20;
     conn_opts.cleansession = 1;
+    //将用户名写入连接选项中
+    conn_opts.username = username; 
+    //将密码写入连接选项中
+    conn_opts.password = password;
+    //使用MQTTClient_connect将client连接到服务器，使用指定的连接选项。成功则返回MQTTCLIENT_SUCCESS
     if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
     {
         printf("Failed to connect, return code %d\n", rc);
         exit(EXIT_FAILURE);
     }
-
+    //消息内容
     pubmsg.payload = PAYLOAD;
+    //消息长度
     pubmsg.payloadlen = (int)strlen(PAYLOAD);
+    //消息质量
     pubmsg.qos = QOS;
+    //消息是否重复发送
     pubmsg.retained = 0;
+    //发布消息
     if ((rc = MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token)) != MQTTCLIENT_SUCCESS)
     {
          printf("Failed to publish message, return code %d\n", rc);
@@ -62,11 +80,13 @@ int main(int argc, char* argv[])
     printf("Waiting for up to %d seconds for publication of %s\n"
             "on topic %s for client with ClientID: %s\n",
             (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
+    //等待消息发布结速
     rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
     printf("Message with delivery token %d delivered\n", token);
-
+    //断开连接
     if ((rc = MQTTClient_disconnect(client, 10000)) != MQTTCLIENT_SUCCESS)
     	printf("Failed to disconnect, return code %d\n", rc);
+    //客户端连接释放
     MQTTClient_destroy(&client);
     return rc;
 }
